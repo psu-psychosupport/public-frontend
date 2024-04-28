7ecbeac2d777ca589bd43b93f548ba12c6d1637e
OKeylo <zabaev228@gmail.com>
2024-04-28 22:52:12 +0500

    feat(routes): add homepage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,43 +1,29 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
+import { Container } from "@mui/material";
+import Header from "../components/header";
+import Search from "~/components/search";
+import PopularQuestions from "~/components/popularQuestions";
+import AllSections from "~/components/allSections";
+import Footer from "~/components/footer";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Стобой" },
+    { name: "description", content: "Добро пожаловать на сайт Стобой!" },
   ];
 };
 
 export default function Index() {
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-      <Link to="/mui">Go to Mui Example</Link>
-    </div>
+    <Container maxWidth={false} disableGutters>
+      <Header />
+      <Container disableGutters sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+        <Search />
+        <PopularQuestions />
+        <AllSections />
+      </Container>
+      <Footer />
+    </Container>
   );
 }
