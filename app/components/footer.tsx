dd1b363e6e910f7ebf818cc6f7a615beeebe0725
OKeylo <zabaev228@gmail.com>
2024-04-28 22:44:13 +0500

    feat(components): add footer

---
+++ app/components/footer.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+
+export default function Footer() {
+  return (
+    <Box height={60} width={"auto"} sx={{ flexGrow: 1, bgcolor: "#303044", mt: "0.25%" }} />
+  );
+}
