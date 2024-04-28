2354a92d13d90bae8bea9d58a87e52c67f9879cb
OKeylo <zabaev228@gmail.com>
2024-04-28 17:16:58 +0500

    feat(components): add search input and button

---
+++ app/components/search.tsx
@@ -0,0 +1,25 @@
+import { styled, alpha } from '@mui/material/styles';
+import { AppBar, Box, Toolbar, Typography, Button, IconButton, Icon, Container, InputBase, TextField  } from "@mui/material";
+import { Menu } from '@mui/icons-material';
+import { Search } from '@mui/icons-material';
+import IconSearch from '~/assets/search';
+
+
+export default function ComponentSearch() {
+  return (
+    <Container disableGutters sx={{display: "flex", alignItems: "center", gap: 2, mt: 2, mb: 4}}>
+      <InputBase
+        className='inputSearch'
+        sx={{ ml: 1, flex: 1, paddingY: 1, paddingX: 2, bgcolor: "#ffffff", borderRadius: "4px", color: "#496CC6" }}
+        fullWidth={true}
+        placeholder="Что интересует?"
+        inputProps={{ 'aria-label': 'search google maps' }}
+      />
+      <IconButton 
+        className='buttonSearch' 
+        children={<IconSearch />} 
+        sx={{bgcolor: 'white', width: 48, height: 48, borderRadius: "4px", boxShadow: 5}}
+      />
+    </Container>
+  );
+}
