8752f4f9965f3a9826789fa6705fc433f1224343
OKeylo <zabaev228@gmail.com>
2024-05-13 16:44:26 +0500

    feat(assets): add Home and Pencil icons

---
+++ app/assets/home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function IconHome(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="31" height="30" viewBox="0 0 31 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M6 18V29H25V18" stroke="#638EFF" stroke-width="2" stroke-linecap="round"/>
+      <path d="M30 17L15.5 2L1 17" stroke="#638EFF" stroke-width="2" stroke-linecap="round"/>
+    </svg>
+  );
+}
+
+export default IconHome;
---
+++ app/assets/pencil.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function IconPencil(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="34" height="33" viewBox="0 0 34 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M27.319 4.5922C28.1984 3.52773 28.0483 1.95194 26.9838 1.07259C25.9194 0.193244 24.3436 0.343318 23.4642 1.40779L27.319 4.5922ZM8.31904 27.5922L27.319 4.5922L23.4642 1.40779L4.46423 24.4078L8.31904 27.5922Z" fill="#638EFF"/>
+      <path d="M1.89163 31L2.8725 26.2768L6.26844 29.2559L1.89163 31Z" fill="#638EFF"/>
+      <path d="M10.3916 30L17.8916 27.5L19.7106 26.8937C21.1913 26.4001 22.7917 27.2003 23.2853 28.681V28.681C23.6184 29.6804 24.7063 30.2125 25.6997 29.8619L32.3916 27.5" stroke="#638EFF" strokeWidth="2" strokeLinecap="round"/>
+    </svg>
+  );
+}
+
+export default IconPencil;
