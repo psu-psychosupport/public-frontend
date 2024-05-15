93a4c144077bcea95da0043d995111431ae3fa3f
OKeylo <zabaev228@gmail.com>
2024-05-15 19:24:09 +0500

    fix(assets\home): incorrect attribute names

--- app/assets/home.tsx
+++ app/assets/home.tsx
@@ -3,8 +3,8 @@ import React from "react";
 function IconHome(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg width="31" height="30" viewBox="0 0 31 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M6 18V29H25V18" stroke="#638EFF" stroke-width="2" stroke-linecap="round"/>
-      <path d="M30 17L15.5 2L1 17" stroke="#638EFF" stroke-width="2" stroke-linecap="round"/>
+      <path d="M6 18V29H25V18" stroke="#638EFF" strokeWidth="2" strokeLinecap="round"/>
+      <path d="M30 17L15.5 2L1 17" stroke="#638EFF" strokeWidth="2" strokeLinecap="round"/>
     </svg>
   );
 }
