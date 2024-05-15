a19fd461f3ee96ab7ff4be622e603c451ac9fb42
OKeylo <zabaev228@gmail.com>
2024-05-15 22:56:16 +0500

    feat(routes\acc-finish): add account finish page

---
+++ app/routes/acc-finish.tsx
@@ -0,0 +1,60 @@
+import type { MetaFunction } from "@remix-run/node";
+import { Container, Typography, Button, Stack } from "@mui/material";
+import { Link } from "@remix-run/react";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Завершение регистрации | Стобой" },
+    { name: "description", content: "Добро пожаловать на сайт Стобой!" },
+  ];
+};
+
+export default function Login() {
+  return (
+    <Container
+      maxWidth={"xs"}
+      sx={{
+        marginTop: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        bgcolor: "white",
+        p: 3,
+        borderRadius: "4px",
+        boxShadow: "0px 0px 7px #638EFF",
+        height: "fit-content",
+        gap: 5
+      }}
+    >
+      <Typography component="h5" variant="h5" color={"#496CC6"} fontSize={30}>
+        Подтверждение аккаунта
+      </Typography>
+
+      <Typography
+      color={"#496CC6"}
+      fontSize={18}
+      pr={4}>
+        Ваш аккаунт был подтверждён!<br/> 
+        Теперь вы можете войти под своим аккаунтом
+      </Typography>
+
+      <Stack alignItems={"center"}>
+        <Link
+          to={"/login"}
+          style={{
+            marginBottom: 2,
+            padding: "2% 20% 2% 20%",
+            width: "fit-content",
+            fontSize: 18,
+            backgroundColor: "#638EFF",
+            boxShadow: "0px 0px 7px #638EFF",
+            textDecoration: "none",
+            borderRadius: "4px",
+            color: "#FFFFFF",
+          }}
+        >
+          Войти
+        </Link>
+      </Stack>
+    </Container>
+  );
+}
