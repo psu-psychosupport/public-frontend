67e83d31dd718d7ee75d6ec026bd70b38ac184fd
Damego <danyabatueff@gmail.com>
2024-05-18 20:54:32 +0500

    refactor: add user icon to header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,21 +1,41 @@
 import { AppBar, Box, Container } from "@mui/material";
-import IconLogo from '~/assets/logo';
-import IconBookmark from '~/assets/bookmark';
+import IconLogo from "~/assets/logo";
+import IconBookmark from "~/assets/bookmark";
 import { Link } from "@remix-run/react";
+import UserIcon from "~/assets/user";
 
 export default function Header() {
   return (
-    <Box sx={{mb: 2}}>
-      <AppBar position="static" sx={{bgcolor: "#303044"}}>
-        <Container sx={{display: "flex", justifyContent: "space-between", alignItems: "center", height: 54}}>
+    <Box sx={{ mb: 2 }}>
+      <AppBar position="static" sx={{ bgcolor: "#303044" }}>
+        <Container
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            height: 54,
+          }}
+        >
           <Link to={"/"}>
             <IconLogo />
           </Link>
 
-          <Box display={'flex'} gap={2}>
+          <Box display={"flex"} gap={2}>
             <IconBookmark />
             <Link to={"/me"}>
-              <Box sx={{ height: 33, width: 33, bgcolor: "primary.main", borderRadius: "4px"}} />
+              <Box
+                sx={{
+                  height: 33,
+                  width: 33,
+                  borderRadius: "4px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "flex-end",
+                  boxShadow: "0px 0px 7px #638EFF",
+                }}
+              >
+                <UserIcon />
+              </Box>
             </Link>
           </Box>
         </Container>
