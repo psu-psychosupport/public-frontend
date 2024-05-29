ac31ef0b7f74b9e242dae9f442e341583d73695c
Damego <danyabatueff@gmail.com>
2024-05-29 12:04:31 +0500

    feat: add user guide doc url to footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,30 @@
-import { Box } from "@mui/material";
+import { Stack } from "@mui/material";
+import { Link } from "@remix-run/react";
+
+const USER_GUIDE_DOCUMENT_URL =
+  "https://docs.google.com/document/d/17VYf9bTCgzXjcp2RUkT0QtjnTmJu8DIc7a49wECi3Hc/edit?usp=sharing";
 
 export default function Footer() {
   return (
-    <Box height={60} width={"auto"} sx={{ bgcolor: "#303044", zIndex: 2 }} />
+    <Stack
+      height={60}
+      width={"auto"}
+      sx={{
+        bgcolor: "#303044",
+        color: "#FFFFFF",
+        zIndex: 2,
+        justifyContent: "center",
+        alignItems: "flex-end",
+      }}
+    >
+      <Link
+        to={USER_GUIDE_DOCUMENT_URL}
+        target={"_blank"}
+        rel={"noreferrer"}
+        style={{ textDecoration: "none", color: "inherit", marginRight: 20 }}
+      >
+        Руководство пользователя
+      </Link>
+    </Stack>
   );
 }
