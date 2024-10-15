d72bcad5f2b4f115bd99e0adfd9bf96466be8a68
Damego <damego.dev@gmail.com>
2024-10-15 18:03:44 +0500

    fix: return null in `getUser` if not authorized

--- app/utils/getUser.ts
+++ app/utils/getUser.ts
@@ -7,6 +7,7 @@ const getUser = async (request: Request) => {
   const userRes = await httpClient.getMe()(request);
   if (userRes.data)
     return redirect("/me", { headers });
+  return null;
 }
 
 export default getUser;
